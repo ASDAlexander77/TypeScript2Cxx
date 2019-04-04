4238c435899350c98cc7864840a79be60edd0da5
Alex D <oleksandr.duzhar@betssongroup.com>
2019-04-04 11:53:50 +0100

    progress of experiments

--- Playground/main.cpp
+++ Playground/main.cpp
@@ -9,12 +9,20 @@ any buildName(any firstName, any secondName = any()) {
 
 int main(int argc, char **argv)
 {
-    std::function<any(any, const std::initializer_list<any> &)> main1 = [&] (any _this, const std::initializer_list<any> &params) ->any {
-        return any();
+    any outer = 12;
+    std::function<any(const std::initializer_list<any> &)> main1 = [&] (const std::initializer_list<any> &params) ->any {
+        auto iter = params.begin();
+        auto end = params.end();
+        any p0 = end != iter ? *iter++ : any();
+        any p1 = end != iter ? *iter++ : any();
+
+        std::cout << outer << std::endl;
+        return p0 + p1;
     };
 
     any o1 = any(anyTypeId::object);
     o1["method"] = buildName;
+    o1["method2"] = main1;
 
     any r = o1["method"](any("First"), any("Second"));
 
@@ -23,6 +31,10 @@ int main(int argc, char **argv)
     any r2 = o1["method"](any("First"));
 
     std::cout << r2 << std::endl;    
+
+    any r3 = o1["method2"](any("p1"), any("p2"));
+
+    std::cout << r3 << std::endl;    
 
     return 0;
 }
