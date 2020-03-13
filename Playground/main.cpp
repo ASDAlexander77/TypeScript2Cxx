c5764519ed2a9965f594a9c1d546f8ba86e53507
Alex D <duzhar@googlemail.com>
2020-03-13 17:45:03 +0000

    progress

--- Playground/main.cpp
+++ Playground/main.cpp
@@ -1,19 +1,61 @@
-#include "core.h"
+#include "test.h"
 
 using namespace js;
 
-std::function<std::function<js::number()>()> f = [&]()
+any f = js::function_t([&]()
 {
-    auto i = 10;
-    return [&]()
+    return 10;
+});
+
+
+template <typename... Types, typename... Args, class = std::enable_if<sizeof...(Types) == sizeof...(Args)>>
+auto conditional_invoke(Args... args)
+{
+    return sizeof...(args);
+}
+
+template <class _Ret, class... _Types>            
+struct _Get_function_args_count {
+    using count = sizeof...(_Types);
+};
+
+template <class F>
+struct _function_t
+{
+    F _f;
+    constexpr static size_t _count = _Get_function_args_count<F>::count;
+
+    _function_t(const F &f) : _f(f)
     {
-        return i;
-    };
+    }
+
+/*
+    template <typename... Args>
+    auto operator()(Args... args) -> decltype(_f(args...)) {
+        return _f(args...);
+    }
+*/    
+
+    template <typename... Args>
+    auto operator()(Args... args) -> int {
+        return sizeof...(args);
+    }
+    
+    auto invoke() -> void {
+
+    }
 };
 
 void Main(void)
 {
-    f()();
+    _function_t f([&]()
+    {
+        return 10;
+    });
+
+    f();
+
+    std::function<void()> ff;
 }
 
 int main(int argc, char** argv)
