131abeaac56a151793d2e57e47d77ba8c8eb8097
Alex D <duzhar@googlemail.com>
2020-03-14 00:55:06 +0000

    progress

--- Playground/main.cpp
+++ Playground/main.cpp
@@ -1,75 +1,16 @@
 #include "core.h"
+#include <any>
 
 using namespace js;
 
-any f = js::function_t([&]()
-{
-    return 10;
-});
-
-
-template <typename... Types, typename... Args, class = std::enable_if<sizeof...(Types) == sizeof...(Args)>>
-auto conditional_invoke(Args... args)
-{
-    return sizeof...(args);
-}
-
-
-template<class _Tx>
-struct _Get_function_info;
-
-template<class _Ret, class... _Types> 
-struct _Get_function_info<_Ret (_Types...)> 
-{	
-    inline static constexpr auto _count = sizeof...(_Types);
-};
-
-template<class _Ret, class... _Types> 
-struct _Get_function_info<_Ret (*)(_Types...)> 
-{	
-    inline static constexpr auto _count = sizeof...(_Types);
-};
-
-
-template <class F>
-struct _function_t
-{
-    F _f;
-    constexpr static size_t _count = _Get_function_info<F>::_count;
-
-    _function_t(const F &f) : _f(f)
-    {
-    }
-
-/*
-    template <typename... Args>
-    auto operator()(Args... args) -> decltype(_f(args...)) {
-        return _f(args...);
-    }
-*/    
-
-    template <typename... Args>
-    auto operator()(Args... args) -> int {
-
-        std::cout << "Hello: " << _count << std::endl;
-
-        return sizeof...(args);
-    }    
-};
-
-void _f_(int a, int b) {
-    
-}
 
 void Main(void)
 {
-    _function_t f(&_f_);
-    f(1, 2);
-
-    _function_t f2([] (int a, int b) {
+    std::any s = std::function([] (int x, int y) {
 
     });
-    f2(1, 2);
+
+    std::invoke(std::any_cast<std::function<void(int, int)>>(s), 1, 2);
 }
 
 int main(int argc, char** argv)
@@ -77,3 +18,4 @@ int main(int argc, char** argv)
     Main();
     return 0;
 }
+ 
