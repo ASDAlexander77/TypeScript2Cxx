4a81a7683198cde86b935c415a09ab5c65e99717
ODuzhar <oduzhar@sportingindex.com>
2020-03-20 00:27:10 +0000

    progress of fixes (test function - union types)

--- test/test.ts
+++ test/test.ts
@@ -1,19 +1,15 @@
-     class Test {                                           
-        private _pointerInput: (p: any, s: any) => void;        
-                                                                
-        public runTest() {                                      
-            this._pointerInput = (p, s) => {                    
-                console.log(p.obj);                             
-                console.log(s.obj);                             
-            };                                                  
-                                                                
-            this.add(this._pointerInput);                       
-        }                                                       
-                                                                
-        public add(callback: (eventData: any, eventState: any) => void) {   
-            callback({ obj: 1 }, { obj: 2});                    
-        }                                                       
-    }                                                           
-                                                                
-    const t = new Test();                                       
-    t.runTest();       
+	 function padLeft(value: string, padding: number)                       
+         function padLeft(value: string, padding: string)                       
+         function padLeft(value: string, padding: any) {                        
+            if (typeof padding == "number") {                                   
+                return padding + value;                                         
+            }                                                                   
+                                                                                
+            if (typeof padding == "string") {                                   
+                return padding + value;                                         
+            }                                                                   
+                                                                                
+            return null;                                                        
+        }                                                                       
+                                                                                
+        console.log(padLeft("Hello world", 4)); 
