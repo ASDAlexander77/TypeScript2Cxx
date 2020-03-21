3d0e1ecc34e0b1b2ae45723c695200c81a954888
ODuzhar <oduzhar@sportingindex.com>
2020-03-21 00:09:13 +0000

    test for cast

--- Playground/main.cpp
+++ Playground/main.cpp
@@ -4,16 +4,15 @@ using namespace js;
 
 void Main(void)
 {
-    auto i = any(1);
-
-/*
     auto f = any([] (int x, int y) -> int {
         std::cout << "Hello" << std::endl;
         return x + y;
     });
 
     auto r = f(1, 2);
-*/    
+
+    std::function<int(int, int)> f2 = f;
+    auto r2 = f2(1, 2);
 }
 
 int main(int argc, char** argv)
