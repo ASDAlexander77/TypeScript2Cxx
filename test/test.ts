b3d21cfb041e65f83e852f909a88a9c68220d286
ODuzhar <oduzhar@sportingindex.com>
2020-03-28 20:33:50 +0000

    progress of testing

--- test/test.ts
+++ test/test.ts
@@ -1,4 +1,11 @@
-	console.log(null === 0);                                               
-        console.log(undefined === 0);                                          
-        console.log(null !== 0);                                               
-        console.log(undefined !== 0);        
+	function _x() {                      
+            console.log(1);                   
+            return 1;                         
+        }                                     
+        function _y() {                       
+            console.log(2);                   
+            return 2;                         
+        }                                     
+                                              
+        console.log(!_x() || _x() == 0 ? _x() : _y());    
+        console.log(_x() && _x() != 0 ? _x() : _y());
