51e921b7ffdf04b12225be5e8dfdf86e350a1d93
ASDAlexander77 <duzhar@googlemail.com>
2020-04-11 17:06:24 +0100

    progress

--- test/test.ts
+++ test/test.ts
@@ -1,8 +1,29 @@
-function f(a)
-{
-	console.log("asd");
+function assertEq(result, expected) {
+  if (result !== expected)
+    throw "Assertion: Expected " + expected + ", got " + result;
 }
 
-var c;
-c=f;
-c(1);
+// Same values as in misc-basic-array-forof.js
+var outer = 500;
+var len = 10000;
+
+function fill_array(len) {
+    var a = Array(len);
+    for (var i = 0; i < outer; i++) {
+	for (var j = 0; j < len; j++)
+	    a[j] = i;
+    }
+    return a;
+}
+
+function use_array(a) {
+    var x = 0;
+    for (var i = 0; i < outer; i++) {
+	for (var j = 0; j < a.length; j++)
+	    x += a[j];
+    }
+    return x;
+}
+
+var a = fill_array(len);
+assertEq(use_array(a), (outer - 1) * outer * len);
