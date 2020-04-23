055418330d71b3b640c60900076ba5fda9b6945c
ASDAlexander77 <duzhar@googlemail.com>
2020-04-23 18:42:07 +0100

    revert

--- test/test.ts
+++ test/test.ts
@@ -1,6 +1,10 @@
-namespace EnumsTest {
-    function testEnums() {
-    }
-
-    testEnums()
-}
+module M {                                 
+            export class C {                        
+                static Y() { return 2; }            
+                                                    
+                X() { return 1; }                   
+            }                                       
+        }                                           
+                                                    
+        const c = new M.C();                        
+        console.log(c.X());
