269a4f70b2365a943325e283d63e9271831a21c6
ASDAlexander77 <duzhar@googlemail.com>
2020-05-30 19:17:05 +0100

    progress

--- Experiment/SimpleVulkanApp/watch_build_cpp.bat
+++ Experiment/SimpleVulkanApp/watch_build_cpp.bat
@@ -1,4 +1,10 @@
+echo off
+
 set START_DIR=%CD%
 cd ..
-call build_cpp.bat
+if not exist __build (call build_cpp.bat)
+cd __build
+echo on
+call msbuild ALL_BUILD.vcxproj /p:Platform=x64 /p:Configuration=Debug /verbosity:quiet
+echo off
 cd %START_DIR%
