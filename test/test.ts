f6b6fc74380637877f20e29845ebef28231ad2ce
ASDAlexander77 <duzhar@googlemail.com>
2023-04-03 14:38:53 +0100

    fixes

--- test/test.ts
+++ test/test.ts
@@ -1,13 +1,3 @@
-	let options = {                                                    
-            b1: false                                                       
-        };                                                                  
-                                                                            
-        let mergedOptions = {                                               
-            bilinearFiltering: false,                                       
-            comparisonFunction: 0,                                          
-            generateStencil: false,                                         
-            ...options                                                      
-        };                                                                  
-                                                                            
-        console.log(mergedOptions.comparisonFunction);                      
-        console.log(mergedOptions.b1);
+	let r;                                     
+        r = ~ false;                                    
+        console.log(r);
