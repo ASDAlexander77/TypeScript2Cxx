cde31068c8200ce045dc0bd568c4d0e608c32f53
ASDAlexander77 <duzhar@googlemail.com>
2026-08-31 17:22:20 +0100

    update clang_test.bat to output testapp1_clang.exe and modify test.bat to include clang_test.bat call

--- test/clang_test.bat
+++ test/clang_test.bat
@@ -1,2 +1,2 @@
-clang++ -std=c++20 -Wno-switch -Wno-deprecated-declarations -Wno-delete-abstract-non-virtual-dtor -I../cpplib test.cpp -o testapp1.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
+clang++ -std=c++20 -Wno-switch -Wno-deprecated-declarations -Wno-delete-abstract-non-virtual-dtor -I../cpplib -o testapp1_clang.exe %1 %2 %3 %4 %5 %6 %7 %8 %9
 del *.o
--- test/test.bat
+++ test/test.bat
@@ -1,1 +1,2 @@
 call ms_test.bat %1 %2 %3 %4 %5 %6 %7 %8 %9
+call clang_test.bat %1 %2 %3 %4 %5 %6 %7 %8 %9
